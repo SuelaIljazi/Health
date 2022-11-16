54cbef1fd21ef50027031e755ace5bc92ce205fe
SuelaIljazi <113540736+SuelaIljazi@users.noreply.github.com>
2022-11-16 17:31:07 +0100

    Health App First Commit

---
+++ Health/Health/HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthApp.swift
+//  Health
+//
+//  Created by Suela Iljazi on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
